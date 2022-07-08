90f7729e5ad6efbb50aab1c592ff7279b5886a81
Franciely Lavine <francielylavine@hotmail.com>
2022-07-07 21:45:51 -0300

    inicio do projeto

---
+++ aula/src/components/Example/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Example() {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default Example;
---
+++ aula/src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+function Menu() {
+    return (
+        <div>
+            <ul className='menu'>
+                <li>Ação</li>
+                <li>Comédia</li>
+                <li>Drama</li>
+                <li>Documentários</li>
+                <li>Infantil</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
